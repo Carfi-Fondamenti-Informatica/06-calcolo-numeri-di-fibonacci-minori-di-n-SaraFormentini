6d2876aa54458ca4e742416442f22ac60d2c9a89
SaraFormentini <127684525+SaraFormentini@users.noreply.github.com>
2023-05-09 22:44:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
+   int  n=0;
+   cin>>n;
+   int fib1=1, fib2=1, fib3, i=0;
+
+    if(n>=2){
+       cout<<fib1<<endl<<fib2<<endl;
+       while(i<=(n-2)) {
+           i++;
+           fib3 = fib1 + fib2;
+           if (fib3<=n) {
+               cout << fib3 << endl;
+           }else{
+               break;
+           }
+           fib1 = fib2;
+           fib2 = fib3;
+           }
+       }
+    else if (n==1){
+        cout<<fib1<<endl<<fib2<<endl;
+    }
    return 0;
 }
